d5c0f8e1e5810e5c940e4b81058c1427b0c1380d
Shaurya Jain <jainshaurya.sj@gmail.com>
2025-08-22 20:59:33 +0530

    behaviour tree node

---
+++ bot_hri/bot_hri/bt.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python3
+#node to test behaviour tree using py_tree
--- bot_hri/bot_hri/rule_engine.py
+++ bot_hri/bot_hri/rule_engine.py
@@ -10,24 +10,15 @@ class RuleEngine(Node):
     def __init__(self):
         super().__init__('rule_engine')
 
-        # Parameters
-        self.threshold_distance = 1.5  # meters
-        self.min_confidence = 50       # % baseline for explanations
-
-        # Subscribers
+        self.threshold_distance = 1.5  
+        self.min_confidence = 50     
         self.create_subscription(LaserScan, '/scan', self.laser_callback, 10)
-
-        # Publisher
         self.publisher_ = self.create_publisher(String, '/bot/log', 10)
 
     def laser_callback(self, msg: LaserScan):
-        # Get min distance from LiDAR ranges
         ranges = np.array(msg.ranges)
         min_dist = np.nanmin(ranges)
-
         event_msg = None
-
-        # Rule 1: Obstacle detected close ahead
         if min_dist < self.threshold_distance:
             confidence = max(60, int(100 - (min_dist / self.threshold_distance) * 100))
             event_msg = {
@@ -36,14 +27,12 @@ class RuleEngine(Node):
                 "conf": confidence
             }
         else:
-            # No obstacle nearby
             event_msg = {
                 "event": "Normal_Drive",
                 "cause": "No obstacle within threshold",
                 "conf": 90
             }
 
-        # Publish JSON
         msg_out = String()
         msg_out.data = json.dumps(event_msg)
         self.publisher_.publish(msg_out)
