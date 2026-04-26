3d61d285f69d89b6f283ba90d9935b730a948e69
Shaurya Jain <jainshaurya.sj@gmail.com>
2026-04-26 18:12:42 +0530

    depth cam

--- bot_description/launch/gazebo.launch.py
+++ bot_description/launch/gazebo.launch.py
@@ -141,7 +141,7 @@ def generate_launch_description():
     ros_gz_image_bridge = Node(
         package="ros_gz_image",
         executable="image_bridge",
-        arguments=["/camera/image_raw"],
+        arguments=["/camera/image"],
         parameters=[{'use_sim_time': True}] 
     )
 
